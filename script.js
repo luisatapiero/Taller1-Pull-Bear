4378fd62f625421238124b54efc5ac8679427669
Miosekai <1010070539@u.icesi.edu.co>
2023-03-10 22:28:00 -0500

    Iconos y Botones

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/*Carrusel ultimos vistos*/ 
+
 function App() {}
 
 window.onload = function (event) {
@@ -34,6 +36,9 @@ let nextAction = (leftPosition,trackWidth,listWidth,slickWidth,track) => {
     }
 }
 
+
+/*carrusel de lovers*/
+
 const carousel = document.querySelector(".carousel"),
 firstImg = carousel.querySelectorAll("img")[0],
 arrowIcons = document.querySelectorAll(".wrapper i");
@@ -41,46 +46,47 @@ arrowIcons = document.querySelectorAll(".wrapper i");
 let isDragStart = false, isDragging = false, prevPageX, prevScrollLeft, positionDiff;
 
 const showHideIcons = () => {
-    // showing and hiding prev/next icon according to carousel scroll left value
-    let scrollWidth = carousel.scrollWidth - carousel.clientWidth; // getting max scrollable width
+    // mostrar y ocultar el icono anterior/siguiente según el valor de desplazamiento izquierdo del carrusel
+    let scrollWidth = carousel.scrollWidth - carousel.clientWidth; // obtener el ancho máximo de desplazamiento
     arrowIcons[0].style.display = carousel.scrollLeft == 0 ? "none" : "block";
     arrowIcons[1].style.display = carousel.scrollLeft == scrollWidth ? "none" : "block";
 }
 
 arrowIcons.forEach(icon => {
     icon.addEventListener("click", () => {
-        let firstImgWidth = firstImg.clientWidth + 1400; // getting first img width & adding 14 margin value
-        // if clicked icon is left, reduce width value from the carousel scroll left else add to it
+        let firstImgWidth = firstImg.clientWidth + 1400; // obteniendo el ancho del primer img y añadiendo el valor del margen 
+        // si el icono pulsado está a la izquierda, reduzca el valor de anchura del desplazamiento del carrusel a la izquierda, si no, añádalo a él
+
         carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
-        setTimeout(() => showHideIcons(), 60); // calling showHideIcons after 60ms
+        setTimeout(() => showHideIcons(), 60); // llamar a showHideIcons después de 60ms
     });
 });
 
 const autoSlide = () => {
-    // if there is no image left to scroll then return from here
+    // si no queda ninguna imagen por desplazar, vuelva desde aquí
     if(carousel.scrollLeft - (carousel.scrollWidth - carousel.clientWidth) > -1 || carousel.scrollLeft <= 0) return;
 
-    positionDiff = Math.abs(positionDiff); // making positionDiff value to positive
+    positionDiff = Math.abs(positionDiff); // hacer que el valor de positionDiff sea positivo
     let firstImgWidth = firstImg.clientWidth + 14;
-    // getting difference value that needs to add or reduce from carousel left to take middle img center
+    // obtener el valor de diferencia que hay que añadir o reducir del carrusel de la izquierda para tomar el centro img medio
     let valDifference = firstImgWidth - positionDiff;
 
-    if(carousel.scrollLeft > prevScrollLeft) { // if user is scrolling to the right
+    if(carousel.scrollLeft > prevScrollLeft) { // si el usuario se desplaza hacia la derecha
         return carousel.scrollLeft += positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
     }
-    // if user is scrolling to the left
+    // isi el usuario se desplaza hacia la izquierda
     carousel.scrollLeft -= positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
 }
 
 const dragStart = (e) => {
-    // updatating global variables value on mouse down event
+    // actualización del valor de las variables globales al pulsar el ratón
     isDragStart = true;
     prevPageX = e.pageX || e.touches[0].pageX;
     prevScrollLeft = carousel.scrollLeft;
 }
 
 const dragging = (e) => {
-    // scrolling images/carousel to left according to mouse pointer
+    // desplazamiento de imágenes/carrusel a la izquierda según el puntero del ratón
     if(!isDragStart) return;
     e.preventDefault();
     isDragging = true;
