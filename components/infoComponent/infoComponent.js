069c8296ea0b6b17478f67ddc7d206a4564128fd
luisatapiero <79235499+luisatapiero@users.noreply.github.com>
2023-04-26 22:43:17 -0500

    Advances in detail product design

--- components/infoComponent/infoComponent.js
+++ components/infoComponent/infoComponent.js
@@ -1,66 +1,55 @@
 import getData from "../../json.js";
 
 const url = window.location.href;
-const page = url.split("/")
-let productname;
-if (page[4] !== "Secondpage.html"){
-    
-const urlNew = window.location.search;
-const pageNew = urlNew.split("/")
-const searchparas = new URLSearchParams(pageNew[0]);
- productname = searchparas.get("id").replace('"', "")
-console.log(productname);
+const page = url.split("/");
+let productname = "";
+if (page[4] !== "Secondpage.html") {
+  const urlNew = window.location.search;
+  const pageNew = urlNew.split("/")
+  const searchparas = new URLSearchParams(pageNew[0]);
+  const id = searchparas.get("id");
+  if (id) {
+    productname = id.replace('"', "");
+  }
+  console.log(productname);
 }
-
 
 
 class InfoComponent extends HTMLElement {
-    static get observedAttributes() {
-        return ['class']
-    }
-    constructor() {
-        super();
-        this.attachShadow({
-            mode: 'open'
-        })
-        this.list = [];
-        this.productDetail;
-    }
-    connectedCallback() {
-        this.printData()
-        this.shadowRoot.innerHTML = `
-        <link rel="stylesheet" href="./components/infoComponent/infoComponent.css">
-        <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-        <div class="title">
+  static get observedAttributes() {
+    return ["class"];
+  }
+  constructor() {
+    super();
+    this.attachShadow({
+      mode: "open",
+    });
+    this.list = [];
+    this.productDetail = null; // Initialize productDetail to null
+  }
+  connectedCallback() {
+    this.printData();
+    this.shadowRoot.innerHTML = `
+      <link rel="stylesheet" href="./components/infoComponent/infoComponent.css">
+      <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+      <div class="title">
         <h3 class="title-text">Detalle de producto</h3>
-        </div>
-        `
-}
+      </div>
+    `;
+  }
 
-    attributeChangeCallback(propName, oldValue, newValue) {
-        this[propName] = newValue;
-        this.printData();
+  attributeChangeCallback(propName, oldValue, newValue) {
+    this[propName] = newValue;
+    this.printData();
+  }
+
+  render() {
+    // Only render if productDetail exists
+    if (!this.productDetail) {
+      return;
     }
 
-    render() {
-
-       /* console.log(this.productDetail.name);
-        let product = this.shadowRoot.createElement("detail-product");
-        product.innerHTML = '';
-        product.innerHTML += `            
-                   
-                
-        <app-productinfo 
-            name="${this.productDetail.name}" 
-            type="${item.type}" 
-            url="${item.url}" 
-            description="${item.description}" 
-            price="${item.price}"
-            color="${item.color}">
-        </app-productinfo>  
-    `*/
-
-    let product = document.createElement("detail-product");
+    const product = document.createElement("detail-product");
     product.innerHTML = `
       <app-productinfo 
         name="${this.productDetail.name}" 
@@ -74,19 +63,17 @@ class InfoComponent extends HTMLElement {
     this.shadowRoot.appendChild(product);
   }
 
-
-    printData() {
-        getData().then((a) => {
-            this.list = a
-            this.productDetail = this.list.find((item) => {
-                let itemCompare = item.name.replaceAll(" ", "-")
-                return itemCompare === productname;
-            });
-            console.log(this.productDetail);
-            this.render()
-        })
-    }
-
+  printData() {
+    getData().then((a) => {
+      this.list = a;
+      this.productDetail = this.list.find((item) => {
+        const itemCompare = item.name.replaceAll(" ", "-");
+        return itemCompare === productname;
+      });
+      console.log(this.productDetail);
+      this.render();
+    });
+  }
 }
 
 customElements.define("app-infoproduct", InfoComponent);
--- components/productComponent/productComponent.js
+++ components/productComponent/productComponent.js
@@ -27,7 +27,7 @@ class ProductComponent extends HTMLElement {
         this.shadowRoot.innerHTML = `
         <link rel="stylesheet" href="./components/productComponent/productComponent.css">
         <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-        <div class="card">
+        <!-- <div class="card">
             <div class="img-container"> 
                 <img src="${this.url}" class="image">
 
@@ -41,10 +41,120 @@ class ProductComponent extends HTMLElement {
                 <p class="color">${this.color}</p>
                 <button class="buy-btn">BUY NOW</button> 
             </div>
+        </div> -->
+
+        <div class="container-back-button">Producto</div>
+
+    
+
+        <main>
+        <div class="container-img">
+            <img
+            src="${this.url}"
+            alt=""
+            />
         </div>
+
+        <div class="container-info-product">
+            <div class="container-price">
+            <h2>${this.name}</h2>
+            <span>$${this.price}</span>
+            </div>
+
+            <div class="container-details-product">
+            <div class="form-group">
+                <label for="colour">Color</label>
+                <select name="colour" id="colour">
+                <option disabled selected value="">Choose an option</option>
+                <option value="Color1">${this.color}</option>
+                <option value="Yellow">Yellow</option>
+                <option value="Blue">Blue</option>
+                </select>
+            </div>
+
+            <div class="form-group">
+                <label for="size">Size</label>
+                <select name="size" id="size">
+                <option disabled selected value="">Choose an option</option>
+                <option value="S">XS</option>
+                <option value="S">S</option>
+                <option value="M">M</option>
+                <option value="L">L</option>
+                <option value="XL">XL</option>
+                </select>
+            </div>
+            </div>
+
+            <div class="container-add-cart">
+            <div class="container-quantity">
+                <input
+                type="number"
+                name=""
+                id=""
+                placeholder="1"
+                value="1"
+                min="1"
+                class="input-quantity"
+                />
+
+                <div class="btn-increment-decrement">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="increment"><path fill="#0092E4" d="M17,13.41,12.71,9.17a1,1,0,0,0-1.42,0L7.05,13.41a1,1,0,0,0,0,1.42,1,1,0,0,0,1.41,0L12,11.29l3.54,3.54a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29A1,1,0,0,0,17,13.41Z"></path></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="decrement"><path fill="#0092E4" d="M17,9.17a1,1,0,0,0-1.41,0L12,12.71,8.46,9.17a1,1,0,0,0-1.41,0,1,1,0,0,0,0,1.42l4.24,4.24a1,1,0,0,0,1.42,0L17,10.59A1,1,0,0,0,17,9.17Z"></path></svg>
+                </div>
+            </div>
+
+            <button class="btn-add-to-cart">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="shopping-bag"><path fill="#0092E4" d="M19,7H16V6A4,4,0,0,0,8,6V7H5A1,1,0,0,0,4,8V19a3,3,0,0,0,3,3H17a3,3,0,0,0,3-3V8A1,1,0,0,0,19,7ZM10,6a2,2,0,0,1,4,0V7H10Zm8,13a1,1,0,0,1-1,1H7a1,1,0,0,1-1-1V9H8v1a1,1,0,0,0,2,0V9h4v1a1,1,0,0,0,2,0V9h2Z"></path></svg>
+                ADD TO BAG
+            </button>
+            </div>
+
+            <div class="container-description">
+            <div class="title-description">
+                <h4>Description</h4>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="angle-down"><path fill="#0092E4" d="M17,9.17a1,1,0,0,0-1.41,0L12,12.71,8.46,9.17a1,1,0,0,0-1.41,0,1,1,0,0,0,0,1.42l4.24,4.24a1,1,0,0,0,1.42,0L17,10.59A1,1,0,0,0,17,9.17Z"></path></svg>
+            </div>
+            
+            <div class="text-description"><p>${this.description}</p></div>
+            </div>
+
+            <div class="container-additional-information">
+            <div class="title-additional-information">
+                <h4>Addtional information</h4>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="angle-down"><path fill="#0092E4" d="M17,9.17a1,1,0,0,0-1.41,0L12,12.71,8.46,9.17a1,1,0,0,0-1.41,0,1,1,0,0,0,0,1.42l4.24,4.24a1,1,0,0,0,1.42,0L17,10.59A1,1,0,0,0,17,9.17Z"></path></svg>
+            </div>
+            <div class="text-additional-information hidden">
+                <p>-------------------------</p>
+            </div>
+            </div>
+
+            <div class="container-reviews">
+                <div class="title-reviews">
+                <h4>Reviews</h4>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="angle-down"><path fill="#0092E4" d="M17,9.17a1,1,0,0,0-1.41,0L12,12.71,8.46,9.17a1,1,0,0,0-1.41,0,1,1,0,0,0,0,1.42l4.24,4.24a1,1,0,0,0,1.42,0L17,10.59A1,1,0,0,0,17,9.17Z"></path></svg>
+                </div>
+                <div class="text-reviews hidden">
+                <p>-------------------------</p>
+                </div>
+            </div>
+
+            <div class="container-social">
+                <span>Compartir</span>
+                <div class="container-buttons-social">
+                    <a href="#"><!-- aqui va icono --></a>
+                    <a href="#"></a>
+                    <a href="#"></a>
+                    <a href="#"></a>
+                </div>
+            </div>
+        </div>
+        </main>
+
+
+
         `;
     }
- 
+
     set url(val) {
         this.setAttribute('url', val);
     }
